11eff7001f9d7de2043264c67f2b766e12c231cf
Robert Schofield <rschofield1992@gmail.com>
2024-04-01 14:27:36 -0400

    commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(1, 2);
+            Add(2.4m, 2.4m);
+            Add(1, 2);
+
+
         }
+
+        public static void Add(int num1, int num2)//exercise 1-----------
+        {
+
+
+            Console.WriteLine(num1 + num2);
+
+        }
+
+        public static void Add(decimal num1, decimal num2)//exercise 2
+        {
+
+
+            {
+                Console.WriteLine(num1 + num2);
+
+            }
+        }
+
+        public static string Add(int num1, int num2, bool check)//exercise 3
+        {
+            var sum = num1 + num2;
+            if (check == true && sum <= 2)
+            { return $"{sum} dollar"; }
+
+            else
+            { return $"{sum} dollar."; }
+
+
+
+        }
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
     }
 }
